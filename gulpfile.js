4121d7f92f527fd5c03fc318bbda39f0c27921e3
Jonathan Ross <jonathan@jibo.com>
2016-08-01 15:23:00 -0400

    Removed gulpfile

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-require('jibo-gulp')(gulp, {
-    src: 'src/**/*.{ts}',
-    tslintSrc: 'src/**/*.ts',
-    typings: 'typings/index.d.ts',
-    target: 'es5',
-    allowJs: false,
-    clean: ['lib']
-});
