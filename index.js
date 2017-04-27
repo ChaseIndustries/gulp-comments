a1291c7d92537bbd763d1e35e7ef6f78d43cbda8
Jordan Chase <chase@chaseu.com>
2017-04-27 11:39:48 -0400

    allowing multiple filters as an array

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 var gutil = require('gulp-util');
 var ext = gutil.replaceExtension;
-
 var through = require('through2');
+
 module.exports = function(filter) {
     return through.obj(function(file, encoding, callback) {
         if (file.isBuffer()) {
             file.path = ext(file.path, '.js');
             var contents = file.contents.toString();
             var comments = contents.match(/[^\S\r\n]*\/(?:\*{2})([\W\w]+?)\*\//gm);
+            var filterComments = function (filter, comments){ 
+              for (var i = comments.length - 1; i >= 0; i--) {
+                if (comments[i].indexOf(filter) >= 0) {
+                    comments.splice(i, 1);
+                }
+              }
+              return comments;
+            }
+
             if(comments) {
-                if (filter) {
-                    for (var i = comments.length - 1; i >= 0; i--) {
-                        if (comments[i].indexOf(filter) >= 0) {
-                            comments.splice(i, 1);
-                        }
-                    }
+                if(filter){ 
+                  if (typeof filter == "string") {
+                    comments = filterComments(filter, comments);
+                  } else if(typeof filter  == "array"){
+                    filter.forEach(function(f){
+                      comments = filterComments(f, comments);
+                    });
+                  }
                 }
                 contents = comments.join('\n\n');
             }
